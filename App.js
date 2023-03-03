b9eb684a0f13c80247a4545fdcbccaaabad270d7
Sabli Jr <sablijr@yahoo.com>
2023-03-03 13:01:33 +0100

    deploying

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ let wordle;
 
 //Fetching the word
 const getWordle = () => {
-  fetch("http://localhost:8000/word")
+  fetch("https://wordleappclone.onrender.com/word")
     .then((response) => response.json())
     .then((json) => {
       // console.log(json);
@@ -135,7 +135,7 @@ const checkRow = () => {
   const guess = guessRows[currentRow].join("");
 
   if (currentTile > 4) {
-    fetch(`http://localhost:8000/check/?word=${guess}`)
+    fetch(`https://wordleappclone.onrender.com/check/?word=${guess}`)
       .then((response) => response.json())
       .then((json) => {
         if (json === false) {
