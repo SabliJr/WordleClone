2ba44eed828368491cd4f75598c13e4ac75c9185
Sabli Jr <sablijr@yahoo.com>
2023-03-03 10:36:39 +0100

    deploying

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:8000"],
+  })
+);
 
 app.get("/word", (req, resp) => {
   const options = {
